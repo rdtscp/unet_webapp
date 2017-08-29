b4e3b1e1f9012df9fc0d4015b2feee11ed0276eb
Alexander Wilson <acwilson96@gmail.com>
2017-08-29 02:09:21 +0100

    Rough Login/Register page created.

---
+++ src/components/login/LoginBox.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+require('./LoginBox.css');
+
+export default class LoginBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { login: 'shown', register: 'hidden' };
+    
+        // This binding is necessary to make `this` work in the callback
+        this.showLogin = this.showLogin.bind(this);
+        this.showRegister = this.showRegister.bind(this);
+        this.register = this.register.bind(this);
+        this.login = this.login.bind(this);
+
+        
+      }
+
+    showLogin() {
+        this.setState({
+            login: 'shown',
+            register: 'hidden'
+        });
+        document.getElementById("login_nav").classList.add("active");
+        document.getElementById("register_nav").classList.remove("active");
+    }
+
+    showRegister() {
+        this.setState({
+            login: 'hidden',
+            register: 'shown'
+        });
+        document.getElementById("register_nav").classList.add("active");
+        document.getElementById("login_nav").classList.remove("active");
+
+    }
+
+    register() {
+        var uname = document.getElementById("reg_uname").value;
+        var pword = document.getElementById("reg_pword").value;
+        
+        $.getJSON("http://localhost:3000/csrfToken",(data, status) => {
+            var csrf = data._csrf;
+            $.post("http://localhost:3000/unet/user/create", {
+                username: uname,
+                password: pword,
+                _csrf: csrf
+            },
+            function(data, status){
+                alert(data.msg);
+            });
+        });
+    }
+
+    login() {
+        var uname = document.getElementById("log_uname").value;
+        var pword = document.getElementById("log_pword").value;
+        
+        $.getJSON("http://localhost:3000/csrfToken",(data, status) => {
+            var csrf = data._csrf;
+            $.post("http://localhost:3000/unet/user/get", {
+                username: uname,
+                password: pword,
+                _csrf: csrf
+            },
+            function(data, status){
+                alert(data.msg);
+            });
+        });
+    }
+
+    render() {
+        return (
+            // Central Card
+            <div className="card login-container">
+                {/* Navbar Container */}
+                <div>
+                    <nav className="nav nav-pills nav-fill">
+                        <a onClick={this.showLogin} id="login_nav" className="nav-item nav-link active" href="javascript:void(0);">Login</a>
+                        <a onClick={this.showRegister} id="register_nav" className="nav-item nav-link" href="javascript:void(0);">Register</a>
+                    </nav>
+                </div>
+                {/* Form Container */}
+                <div>
+                    <div className={this.state.login} id="login_div">
+                        <div className="form-group">
+                            <input id="log_uname" type="username" className="form-control" aria-describedby="emailHelp" placeholder="Username" />
+                        </div>
+                        <div className="form-group">
+                            <input id="log_pword" type="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <button onClick={this.login} className="btn btn-primary justify-content-center">Login</button>
+                    </div>
+                    <div className={this.state.register} id="register_div">
+                        <div className="form-group">
+                            <input id="reg_uname" type="username" className="form-control" aria-describedby="emailHelp" placeholder="Username" />
+                        </div>
+                        <div className="form-group">
+                            <input id="reg_pword" type="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <button onClick={this.register} className="btn btn-primary justify-content-center">Register</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
