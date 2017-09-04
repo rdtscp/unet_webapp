09a53126ab52ea34cb3e4f493c6aa34890cf40e0
Alex Wilson <acwilson96@protonmail.com>
2017-09-04 14:26:33 +0100

    Unstyled, temporary logout button for testing.

--- src/components/ChatApp/LeftPane/Overlay/BurgerOverlay/BurgerOverlay.js
+++ src/components/ChatApp/LeftPane/Overlay/BurgerOverlay/BurgerOverlay.js
@@ -8,10 +8,18 @@ export default class BurgerOverlay extends Component {
     constructor(props) {
         super(props);
     }
+    
+    logout() {
+        localStorage.removeItem('token');
+        window.location.reload()
+    }
 
     render() {
         return (
+            <div id="BurgerMenu">
             <Header title="More Apps Coming Soon!" close={this.props.close} />
+            <a onClick={this.logout}> logout </a>
+            </div>
         );
     }
 }
