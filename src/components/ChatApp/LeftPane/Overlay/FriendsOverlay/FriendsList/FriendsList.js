880a45615980a2e64ef067edf6c52d916ba0953e
Alexander Wilson <acwilson96@gmail.com>
2017-09-04 14:01:21 +0100

    New files moved.

---
+++ src/components/ChatApp/LeftPane/Overlay/FriendsOverlay/FriendsList/FriendsList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './FriendsList.css';
+
+export default class FriendsList extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="friendsListEntry">
+            </div>
+        );
+    }
+}
