24a3f146002c7245ec3f54f1495a3efb2680d445
Alex Wilson <acwilson96@protonmail.com>
2017-09-04 02:04:08 +0100

    Networking and rendering for FriendList population. ~ROUGH

--- src/components/ChatApp/LeftPane/Overlays/FriendsOverlay.js
+++ src/components/ChatApp/LeftPane/Overlays/FriendsOverlay.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import './Overlay.css';
 
+import axios from 'axios';
+import network from './networkHelper.js';
+
 import Header from './Header/Header.js';
 
 export default class FriendsOverlay extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            friends: []
+        }
     }
 
+    // Fetches all Friendships.
+    componentDidMount () {
+        
+        var token = localStorage.getItem('token');
+    
+        // Get CSRF token.
+        network.getCSRF((csrf) => {
+            axios({
+                method: 'POST',
+                url: 'http://api.localhost:1337/unet/friendships/get/all',
+                data: {
+                    _csrf: csrf,
+                    token: token
+                },
+                withCredentials: true,
+                contentType: 'json',
+            })
+            .then((response) => {
+                alert(response.data.msg)
+                if (response.data.friendships) {
+                    // this.setState({
+                    //    friends: response.data.friendships
+                    // });
+                }
+            });
+        });
+    
+      }
+
     render() {
+        this.state.friends.push({sender: 'Add Friend'});
+        var friends = this.state.friends.map((friend) => {
+            return  (
+                <li> {friend.sender} </li>
+            );
+        });
         return (
             <div className="overlay">
                 <Header title="Friends List" close={this.props.toggleFriends} />
+                <ul>
+                    {friends}
+                </ul>
             </div>
         );
     }
