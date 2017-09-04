c669625c4616f278efa274c9d09350f60d9241fe
Alex Wilson <acwilson96@protonmail.com>
2017-09-04 14:37:38 +0100

    FriendsList component now retrieves a list of friendships from API.

--- src/components/ChatApp/LeftPane/Overlay/FriendsOverlay/FriendsList/FriendsList.js
+++ src/components/ChatApp/LeftPane/Overlay/FriendsOverlay/FriendsList/FriendsList.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import './FriendsList.css';
 
+import network from './networkHelper.js';
+import axios from 'axios';
+
 export default class FriendsList extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            friends: []
+        }
+    }
+
+    componentDidMount () {
+        
+        var token = localStorage.getItem('token');
+        // Get CSRF token.
+        network.getCSRF((csrf) => {
+            // Get Friends.
+            axios({
+                method:'POST',
+                url:'http://api.localhost:1337/unet/friendship/get/all',
+                data: {
+                  _csrf: csrf,
+                  token: token
+                },
+                withCredentials: true,
+                contentType: 'json',
+            })
+            .then((response) => {
+                if (response.data.msg) alert(response.data.msg);
+                this.setState({
+                    friends: response.data.friendships
+                });
+            })
+        });
+    
     }
 
     render() {
+
+        const listItems = this.state.friends.map((friend) =>
+            // Create friend entry, pass data down to props.
+            <li>{friend.id}</li>
+        );
+
         return (
-            <div className="friendsListEntry">
+            <div className="friendsListBody">
+                {listItems}
             </div>
         );
     }
