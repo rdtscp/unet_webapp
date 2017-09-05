25ffd2790dbbfbed80d7c59d8067a6cbbd6e6bd1
Alexander Wilson <acwilson96@gmail.com>
2017-09-05 16:43:21 +0100

    Methods and state for creating a chat.

--- src/components/ChatApp/LeftPane/Overlay/ComposeOverlay/ComposeOverlay.js
+++ src/components/ChatApp/LeftPane/Overlay/ComposeOverlay/ComposeOverlay.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
 
 import Header from '../Header/Header.js';
+import FriendsList from './FriendsList/FriendsList.js';
+
+import network from './networkHelper.js';
+import axios from 'axios';
 
 export default class ComposeOverlay extends Component {
     
     constructor(props) {
         super(props);
+        // Have no members in chat.
+        this.state = {
+            chatMembers: []
+        }
+        console.log(this.state.chatMembers)
+    }
+
+    // Add a member to the state.
+    addChatMember = (member) => {
+        this.state.chatMembers.push(member.friend);
+    }
+
+    // Remove a member from the state.
+    rmChatMember = (member) => {
+        var chatMembers     = this.state.chatMembers.splice(0,0);
+        var index           = chatMembers.indexOf(member.friend);
+        this.state.chatMembers.splice(index, 1);
+    }
+
+    // Post to backend to create the chat.
+    createChat = () => {
+        console.log(this.state.chatMembers)
+        var token = localStorage.getItem('token');
+        network.getCSRF((csrf) => {
+            axios({
+                method:'POST',
+                url:'http://api.localhost:1337/unet/chat/create',
+                data: {
+                  _csrf: csrf,
+                  token: token,
+                  members: this.state.chatMembers
+                },
+                withCredentials: true,
+                contentType: 'json',
+            })
+            .then((response) => {
+                console.log(response);
+            })
+        });
     }
 
     render() {
         return (
-            <Header title="New Chat" close={this.props.close} />
+            <div id="FriendsOverlay">
+                <Header type="compose" title="New Chat" close={this.props.close} createChat={this.createChat} />
+                <FriendsList addChatMember={this.addChatMember} rmChatMember={this.rmChatMember} />
+            </div>
         );
     }
 }
