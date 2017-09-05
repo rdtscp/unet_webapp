902eca1957d06862c87468ffb7c88fd0a3def492
Alexander Wilson <acwilson96@gmail.com>
2017-09-05 21:56:55 +0100

    Chat list fetches data from API.

--- src/components/ChatApp/LeftPane/ListBody/ListBody.js
+++ src/components/ChatApp/LeftPane/ListBody/ListBody.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import './ListBody.css';
 
+import axios from 'axios';
+import network from './networkHelper.js';
+
 export default class ListBody extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            chats: []
+        }
+    }
+
+    // Retrieve list of Chats, and set the state.
+    componentDidMount () {
+        var token = localStorage.getItem('token');
+        // Get CSRF token.
+        network.getCSRF((csrf) => {
+            // Get Friends.
+            axios({
+                method:'POST',
+                url:'http://api.localhost:1337/unet/user/get',
+                data: {
+                  _csrf: csrf,
+                  token: token
+                },
+                withCredentials: true,
+                contentType: 'json',
+            })
+            .then((response) => {
+                if (response.data.chats) {
+                    this.setState({
+                        chats: response.data.chats
+                    });
+                }
+            })
+        });
     }
 
     render() {
