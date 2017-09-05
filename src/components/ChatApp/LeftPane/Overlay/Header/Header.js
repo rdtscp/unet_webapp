82dd4521e155e62006efdb9cff5bdbaa8dc25a00
Alex Wilson <acwilson96@protonmail.com>
2017-09-05 16:42:45 +0100

    Create Chat included in header now.

--- src/components/ChatApp/LeftPane/Overlay/Header/Header.js
+++ src/components/ChatApp/LeftPane/Overlay/Header/Header.js
@@ -11,12 +11,22 @@ export default class Header extends Component {
     }
 
     render() {
+
+        var create = null;
+        if (this.props.type == "compose") {
+            create =  <div className="createChatBut" onClick={this.props.createChat}>
+                          <a className="button is-info" >Create</a>
+                      </div>
+        }
+
+
         return (
             <div className="header">
                 <CloseBut close={this.props.close} />
                 <div className="headerTitle">
                     {this.props.title}
                 </div>
+                {create}
             </div>
         );
     }
