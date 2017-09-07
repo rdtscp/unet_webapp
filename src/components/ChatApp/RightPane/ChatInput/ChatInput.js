65255116e20da839126b8aa2f48bb004140127bc
Alexander Wilson <acwilson96@gmail.com>
2017-09-07 23:44:20 +0100

    Some minor JS to handle keypresses in input field.

--- src/components/ChatApp/RightPane/ChatInput/ChatInput.js
+++ src/components/ChatApp/RightPane/ChatInput/ChatInput.js
@@ -7,6 +7,27 @@ export default class ChatInput extends Component {
         super(props);
     }
 
+    componentDidMount() {
+        document.addEventListener('keydown', this._handleKeyPress);
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this._handleKeyPress);
+    }
+
+    _handleKeyPress = (e) => {
+        var input = document.getElementById('input');
+        if (e.keyCode === 13 && !e.shiftKey) {
+            e.preventDefault();
+            var msg = input.innerHTML;
+            alert(msg)
+        }
+        else if (e.keyCode == 38 && input.innerHTML == '') {
+            alert('Get latest message.');
+            //document.getElementById('input').innerHTML = (document.getElementById(latest_msg +  '_content').innerHTML);
+        }
+    }
+
     render() {
         return (
             <div className="chat-footer">
