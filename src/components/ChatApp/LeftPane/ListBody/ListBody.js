482601f5012acaa5484a66df951516469713a0c2
Alexander Wilson <acwilson96@gmail.com>
2017-09-09 11:22:43 +0100

    ListBody now gets its chat list from parent components.

--- src/components/ChatApp/LeftPane/ListBody/ListBody.js
+++ src/components/ChatApp/LeftPane/ListBody/ListBody.js
@@ -15,36 +15,10 @@ export default class ListBody extends Component {
         }
     }
 
-    // Retrieve list of Chats, and set the state.
-    componentDidMount () {
-        var token = localStorage.getItem('token');
-        // Get CSRF token.
-        network.getCSRF((csrf) => {
-            // Get Friends.
-            axios({
-                method:'POST',
-                url:'http://api.localhost:1337/unet/user/get',
-                data: {
-                  _csrf: csrf,
-                  token: token
-                },
-                withCredentials: true,
-                contentType: 'json',
-            })
-            .then((response) => {
-                if (response.data.user.chats) {
-                    this.setState({
-                        chats: response.data.user.chats
-                    });
-                }
-            })
-        });
-    }
-
     render() {
 
         // Create a list of FriendEntrys.
-        const chatList = this.state.chats.map((chat) =>
+        const chatList = this.props.chats.map((chat) =>
             // Create friend entry, pass data and remove method down.
             <ChatEntry data={chat} key={chat.id} openChat={this.props.openChat} />
         );
