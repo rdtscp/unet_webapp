e3427e8efe2185410cc762dac7d609ed1b48b11f
Alex Wilson <acwilson96@protonmail.com>
2018-06-01 00:38:49 +0100

    Heroku Deployment

--- heroku.js
+++ heroku.js
@@ -9,7 +9,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+    console.log('Webpage Requested by IP: ' + (req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+  res.sendFile(path.join(__dirname+'/build/'));
 });
 
 const port = process.env.PORT || 5000;
