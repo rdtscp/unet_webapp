eca977c1cfd74948fd0351a8a6ca5eabac4ad734
Alexander Wilson <acwilson96@gmail.com>
2018-05-31 22:22:37 +0100

    Clean Up

--- src/components/ChatApp/RightPane/ChatHeader/ChatHeader.js
+++ src/components/ChatApp/RightPane/ChatHeader/ChatHeader.js
@@ -4,15 +4,11 @@ import './ChatHeader.css';
 
 export default class ChatHeader extends Component {
     
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="chatHeader">
                 <div className="chatAvatar">
-                    <img src="http://bulma.io/images/placeholders/64x64.png" alt="Image" />
+                    <img src="http://bulma.io/images/placeholders/64x64.png" />
                 </div>
                 <div className="chatName is-size-3">
                         {this.props.chat.name}
