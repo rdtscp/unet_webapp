8ac310ef823bbec9ad35577fcd8365cb91165bb3
Alex Wilson <acwilson96@protonmail.com>
2018-06-01 01:05:18 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+// import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
