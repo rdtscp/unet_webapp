90ba7a55d3cf833bde9d7737e778a762eff8c95f
Alexander Wilson <acwilson96@gmail.com>
2018-08-15 17:42:35 +0100

    Enforce HTTPS

--- src/index.js
+++ src/index.js
@@ -6,9 +6,5 @@ import 'font-awesome/css/font-awesome.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-if (location.protocol !== process.env.REACT_APP_PROTOCOL) {
-  location.href = process.env.REACT_APP_PROTOCOL + window.location.href.substring(window.location.protocol.length);
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
