0d920980ce2571d48dd2b78e52d2874ac0c73fbd
Alexander Wilson <acwilson96@gmail.com>
2018-08-15 17:38:27 +0100

    Force HTTPS in Production

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ import 'font-awesome/css/font-awesome.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (location.protocol !== process.env.REACT_APP_PROTOCOL) {
+  location.href = process.env.REACT_APP_PROTOCOL + window.location.href.substring(window.location.protocol.length);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
